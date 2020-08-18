e02764c9f1afeb16b9dbdf30b371b0db197e8d57
Michael Bagnall <mrbagnall@icloud.com>
2020-08-17 21:09:11 -0400

    Complete re-write of the node enable interface. Working, but not tied to grant form yet.

--- src/AccessChecks/NodeGrantAccessCheck.php
+++ src/AccessChecks/NodeGrantAccessCheck.php
@@ -6,6 +6,7 @@ use Drupal\Core\Routing\Access\AccessInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Access\AccessResult;
 use Drupal\node\Entity\Node;
+use Drupal\node\Entity\NodeType;
 
 /**
  * A custom access check for grants form.
@@ -16,19 +17,23 @@ class NodeGrantAccessCheck implements AccessInterface {
    * A custom access check.
    */
   public function access($node, AccountInterface $account) {
+
     if (!$node) {
       return AccessResult::forbidden();
     }
     $nid = $node;
     $node = Node::load($nid);
+    $bundle = $node->getType();
 
-    $config = \Drupal::configFactory()->get('nodeaccess.settings');
-    $allowed_types = $config->get('allowed_types');
-    if ($node && isset($allowed_types[$node->getType()]) && !empty($allowed_types[$node->getType()]) &&
-        ($account->hasPermission('grant node permissions') || $account->hasPermission('administer nodeaccess'))) {
+    $interface =  NodeType::load($bundle);
+    $granted = $interface->getThirdPartySetting('nodeaccess', 'nodeaccess_grant_tab_' . $bundle, FALSE);
+
+    if (!empty($granted)) {
       return AccessResult::Allowed();
     }
-    return AccessResult::forbidden();
+    else {
+      return AccessResult::forbidden();
+    }
   }
 
 }
